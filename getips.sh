0b2e2d73be424f817ced8ae0b89a61f7446baa1b
f0xtty <qxfabianxq@hotmail.com>
2021-02-10 22:13:20 -0300

    replace replaced with sed

--- getips.sh
+++ getips.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ifconfig | grep  "inet " -B 1 | sed 's/:.*//g' | sed 's/netmask.*//g' | sed 's/\n*inet/: /g' | tr -d ' ' | tr '\n' ' ' | sed 's/ -- /\n/g' | replace ' :' ': '; echo
+ifconfig | grep  "inet " -B 1 | sed 's/:.*//g' | sed 's/netmask.*//g' | sed 's/\n*inet/: /g' | tr -d ' ' | tr '\n' ' ' | sed 's/ -- /\n/g' | sed 's/ \:/\: /g' ; echo
